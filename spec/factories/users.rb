ddfaa5574c61005c58129daf3210af7959c72cad
LMarianow <semalus2011@gmail.com>
2023-02-01 14:49:24 -0300

    somo tests in rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
-    email { Faker::Name.name }
-    password_digest { Faker::Name.name  }
-    cpf { Faker::Name.name  }
+    email { Faker::Internet.email}
+    password_digest { Faker::Crypto.sha1  }
+    cpf { Faker::Alphanumeric.alpha(number: 10)  }
   end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -16,4 +16,57 @@ RSpec.describe Account do
 
     it { is_expected.to be_persisted }
   end
+
+  describe 'instance methods' do
+    let!(:account2) { create(:account) }
+
+    context '#deposit' do
+      before { account2.deposit(50) }
+
+      it 'must be deposited the value' do
+        expect(account2.reload.balance).to eq(50)
+      end
+    end
+
+    context '#withdraw' do
+      let!(:account2) { create(:account, balance: 200) }
+
+      context 'account have balance suficient' do
+        before { account2.withdraw(50) }
+
+        it 'must be withdraw the value' do
+          expect(account2.reload.balance).to eq(150)
+        end
+      end
+
+      context 'account dont have balance suficient' do
+        it 'dont must be withdraw the value' do
+          expect{account2.withdraw(500)}.to raise_error(ArgumentError)
+        end
+      end
+    end
+  end
+
+  describe '#transference' do
+    context 'account have balance suficient' do
+      let!(:account2) { create(:account, balance: 200) }
+      let!(:account3) { create(:account, balance: 500) }
+
+      before { 
+        account3.reload
+        account2.transference(100, account3.user.email) }
+
+      it 'must be transference the value', :aggregate_failures do
+        expect(account2.reload.balance).to eq(100)
+        byebug
+        expect(Account.last.reload.balance).to eq(500)
+      end
+    end
+
+    context 'account dont have balance suficient' do
+      it 'dont must be transference the value' do
+        expect{account2.transference(5000, account3.user.email)}.to raise_error(ArgumentError)
+      end
+    end
+  end
 end
